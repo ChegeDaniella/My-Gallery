9d3a07c690c1e3544f455aca912a5bb31e2e2e76
ChegeDaniella <chegedaniella@gmail>
2020-05-22 15:19:34 +0300

    adding image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+ 
 
-# Create your models here.
+class Image(models.Model):
+   
+    image_name = models.CharField(max_length = 60)
+    image_description = models.TextField (max_length = 500)
+    image_location = models.CharField(max_length = 60)
+    image_category = models.CharField(max_length = 60)
